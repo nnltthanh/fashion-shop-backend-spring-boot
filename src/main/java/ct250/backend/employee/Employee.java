c063529d7529a102b4e03606553488ef673d6294
lytantai <lytantai02@gmail.com>
2024-01-23 21:08:31 +0700

    create models (Coupon, Customer, Employee, Manager, Staff)

---
+++ src/main/java/ct250/backend/employee/Employee.java
@@ -0,0 +1,54 @@
+package ct250.backend.employee;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@Component
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "employee")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 20)
+    private String username;
+
+    @Column(length = 30)
+    private String password;
+
+    @Column(length = 50)
+    private String name;
+
+    @Column
+    private String address;
+
+    @Column(length = 12)
+    private String phone;
+
+    @Column(length = 80)
+    private String email;
+
+    @Column(name = "avatarURL")
+    private String avatar;
+
+    @Temporal(value = TemporalType.DATE)
+    private Date dob;
+
+    @Column(name = "created_at")
+    @Temporal(value = TemporalType.TIMESTAMP)
+    private Date createdAt;
+
+    @Column(name = "updated_at")
+    @Temporal(value = TemporalType.TIMESTAMP)
+    private Date updatedAt;
+}
---
+++ src/main/java/ct250/backend/staff/Staff.java
@@ -0,0 +1,21 @@
+package ct250.backend.staff;
+
+import ct250.backend.employee.Employee;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.stereotype.Component;
+
+@Setter
+@Getter
+@Component
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "staff")
+public class Staff extends Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
