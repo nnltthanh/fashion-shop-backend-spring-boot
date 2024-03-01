09df702b718bb6174d4030f0af6f85407d7c9028
lytantai <lytantai02@gmail.com>
2024-03-01 16:38:09 +0700

    update: sql sample (product, image_data)

--- sql-sample/image_data.sql
+++ sql-sample/image_data.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Mar 01, 2024 at 08:18 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th3 01, 2024 lúc 10:36 AM
+-- Phiên bản máy phục vụ: 10.4.32-MariaDB
+-- Phiên bản PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `fashion-shop`
+-- Cơ sở dữ liệu: `fashion-shop`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `image_data`
+-- Cấu trúc bảng cho bảng `image_data`
 --
 
 CREATE TABLE `image_data` (
@@ -34,7 +34,7 @@ CREATE TABLE `image_data` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `image_data`
+-- Đang đổ dữ liệu cho bảng `image_data`
 --
 
 INSERT INTO `image_data` (`id`, `base64string`, `type`) VALUES
@@ -44,27 +44,35 @@ INSERT INTO `image_data` (`id`, `base64string`, `type`) VALUES
 (4, 'https://media.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85,format=auto/uploads/January2024/photic3d.1.jpg', 'PRODUCT'),
 (5, 'https://media.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85,format=auto/uploads/October2023/sCM006.thumb1.2.jpg', 'PRODUCT'),
 (6, 'https://media.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85,format=auto/uploads/October2023/th2_CMM0218.jpg', 'PRODUCT'),
-(7, 'https://media.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85/uploads/February2024/polopromax-s1.26.jpg', 'PRODUCT');
+(7, 'https://media.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85/uploads/February2024/polopromax-s1.26.jpg', 'PRODUCT'),
+(8, 'https://media.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85,format=auto/uploads/December2023/joggerut.5.jpg', 'PRODUCT'),
+(9, 'https://media.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85,format=auto/uploads/November2023/23CMCW.JE002.7.jpg', 'PRODUCT'),
+(10, 'https://media.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85,format=auto/uploads/December2023/sao_giu_nhiet_trang_6.jpg', 'PRODUCT'),
+(11, 'https://media.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85,format=auto/uploads/November2023/23CMAW.QD004.11k.jpg', 'PRODUCT'),
+(12, 'https://media.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85,format=auto/uploads/February2024/mockup4.jpg', 'PRODUCT'),
+(13, 'https://media.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85,format=auto/uploads/April2023/Ao_thun_cotton_in_Care_Share_moi-6.jpg', 'PRODUCT'),
+(14, 'https://media.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85,format=auto/uploads/October2023/denn3-(2)_copys.jpg', 'PRODUCT'),
+(15, 'https://media.coolmate.me/cdn-cgi/image/width=672,height=990,quality=85,format=auto/uploads/November2021/1426x2100.jpg', 'PRODUCT');
 
 --
--- Indexes for dumped tables
+-- Chỉ mục cho các bảng đã đổ
 --
 
 --
--- Indexes for table `image_data`
+-- Chỉ mục cho bảng `image_data`
 --
 ALTER TABLE `image_data`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT cho các bảng đã đổ
 --
 
 --
--- AUTO_INCREMENT for table `image_data`
+-- AUTO_INCREMENT cho bảng `image_data`
 --
 ALTER TABLE `image_data`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
