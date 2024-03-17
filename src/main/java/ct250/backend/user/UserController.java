349385404891b0fe428eed9ba1ace168e9444593
Lam Thanh <thanhreika2002@gmail.com>
2024-03-17 16:38:57 +0700

    Remove unused import

--- src/main/java/ct250/backend/user/UserController.java
+++ src/main/java/ct250/backend/user/UserController.java
@@ -1,13 +1,17 @@
 package ct250.backend.user;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import ct250.backend.customer.Customer;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
